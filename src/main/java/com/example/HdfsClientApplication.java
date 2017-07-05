0fd22a47c85adf6426399203877a43b32a5b9940
ZhangBing <v_zhangbing@baidu.com>
2017-07-05 15:43:48 +0800

    Initial commit

---
+++ src/main/java/com/example/HdfsClientApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfsClientApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/HdfsClientTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by v_zhangbing on 2017/7/4.
+ */
+public class HdfsClientTest {
+
+    private Configuration conf;
+    private FileSystem fs;
+
+    @Before
+    public void init() throws URISyntaxException, IOException, InterruptedException {
+        conf = new Configuration();
+        //conf.set("fs.defaultFS","hdfs://ubuntu:9000");
+
+        fs = FileSystem.get(new URI("hdfs://ubuntu:9000"), conf, "zb");
+    }
+
+    @Test
+    public void testUploadFile() throws IOException {
+        fs.copyFromLocalFile(new Path("C:/Users/v_zhangbing/Downloads/aaa"), new Path("/java/aaa"));
+        fs.close();
+    }
+
+
+}
